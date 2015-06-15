3cac83ee876c16c439125442b9edaf843b61039b
Dennis Leon <dennis.leon39@gmail.com>
2015-06-14 23:41:19 -0700

    initial attempt at q1

--- ex_1/hello-1.c
+++ ex_1/hello-1.c
@@ -6,7 +6,7 @@
 
 int init_module(void)
 {
-	printk(KERN_INFO "Hello world 1.\n");
+	printk(KERN_DEBUG "Hello World! 1.\n");
 
 	/* 
 	 * A non 0 return means init_module failed; module can't be loaded. 
